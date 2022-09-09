547b9d41a918e43dfe98cbfe52f8f584f50d1aeb
jeison poveda <jeisonfpovedac@gmail.com>
2022-09-08 20:15:29 -0500

    Finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "ask", to: "pages#ask"
+  get "answer", to: "pages#answer"
   # Defines the root path route ("/")
+  root to: "pages#home"
   # root "articles#index"
 end
